e40f4032df1b3315fbb0045d00204af45e7ee357
Axel Miesen <mail@am42.de>
2021-11-25 07:31:07 +0100

    Minor improvements

--- awx/setup.sh
+++ awx/setup.sh
@@ -4,7 +4,7 @@ AWX_VERSION=17.1.0
 AWX_ADMIN_PASSWORD=password
 
 
-git clone -b $AWX_VERSION https://github.com/ansible/awx.git
+test -d awx || git clone -b $AWX_VERSION https://github.com/ansible/awx.git
 
 pushd awx/installer
 
@@ -20,7 +20,8 @@ ansible-playbook modify-compose-file.yml
 cd ~/.awx/awxcompose
 docker-compose run --rm --service-ports task awx-manage migrate --no-input
 
-docker-compose up -d --remove-orphans
+sleep 10
+docker-compose up -d
 
 # Load demo data:
 # docker exec awx_task bash -c "/usr/bin/awx-manage create_preload_data"
